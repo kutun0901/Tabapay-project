3e8999f9dafdfff50c08e6ec33d4e971a2559657
kutun0901 <tuan.tq2433@gmail.com>
2024-03-12 14:33:23 -0700

    created treeMenu part1

---
+++ front-end/src/components/treeMenu/TreeMenu.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import TreeNode from './TreeNode';
+
+const treeSample = [
+    {
+        title: 'Root',
+        children: [
+            {
+                title: 'Parent A',
+                children: [
+                    { title: 'Child A1', children: [] },
+                    {
+                        title: 'Child A2', children: [
+                            {
+                                title: 'Child A21', children: []
+                            },
+                            {
+                                title: 'Child A22', children: []
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                title: 'Parent B',
+                children: [
+                    {title: 'Child B1', children: []},
+                    {title: 'child B2', children: []},
+                    {title: 'Child Parent B3', children: []}
+                ]
+            }
+
+        ]
+    }
+]
+
+const TreeMenu = () => {
+
+    return (
+        <div>
+            {treeSample.map((node, index) => (
+                <TreeNode key={index} node={node}/>
+            ))}
+        </div>
+    )
+}
+
+export default TreeMenu;
