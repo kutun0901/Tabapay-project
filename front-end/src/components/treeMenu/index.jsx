4fb34fb0998c1bd0c706a504bacca3dccb7e0f62
kutun0901 <tuan.tq2433@gmail.com>
2024-03-13 21:29:29 -0700

    set up server to connect frontend part-6

--- front-end/src/components/treeMenu/index.jsx
+++ front-end/src/components/treeMenu/index.jsx
@@ -1,43 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TreeNode from './TreeNode';
 
-const treeSample = [
-    {
-        title: 'Root',
-        children: [
-            {
-                title: 'Parent A',
-                children: [
-                    { title: 'Child A1', children: [] },
-                    {
-                        title: 'Child A2', children: [
-                            {
-                                title: 'Child A21', children: []
-                            },
-                            {
-                                title: 'Child A22', children: []
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                title: 'Parent B',
-                children: [
-                    {title: 'Child B1', children: []},
-                    {title: 'Child B2', children: []},
-                    {title: 'Child Parent B3', children: []}
-                ]
-            }
+// const treeSample = [
+//     {
+//         title: 'Root',
+//         children: [
+//             {
+//                 title: 'Parent A',
+//                 children: [
+//                     { title: 'Child A1', children: [] },
+//                     {
+//                         title: 'Child A2', children: [
+//                             {
+//                                 title: 'Child A21', children: []
+//                             },
+//                             {
+//                                 title: 'Child A22', children: []
+//                             }
+//                         ]
+//                     }
+//                 ]
+//             },
+//             {
+//                 title: 'Parent B',
+//                 children: [
+//                     {title: 'Child B1', children: []},
+//                     {title: 'Child B2', children: []},
+//                     {title: 'Child Parent B3', children: []}
+//                 ]
+//             }
 
-        ]
-    }
-]
+//         ]
+//     }
+// ]
 
 const TreeMenu = ({ onItemClick }) => {
+
+    const [treeData, setTreeData] = useState([]);
+
+    useEffect(() => {
+        fetchTreeData();
+    }, []);
+
+    const fetchTreeData = async () => {
+        try {
+            const response = await fetch('/api/tree');
+            if (!response.ok) {
+                throw new Error('Failed to fetch tree data');
+            }
+            const data = await response.json();
+
+            // console.log(data)
+
+            setTreeData(data);
+        } catch (error) {
+            console.error('Error fetching tree data:', error);
+        }
+    };
+
     return (
         <div>
-            {treeSample.map((node, index) => (
+            {treeData.map((node, index) => (
                 <TreeNode key={index} node={node} onItemClick={onItemClick} />
             ))}
         </div>
